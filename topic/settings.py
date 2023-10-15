5da52a8a90909031a0cacfbecc9e44d4b1bdd05b
anbarOwO <102491759+anbarOwO@users.noreply.github.com>
2023-10-15 13:53:08 +0800

    修正myqsl 連接流程

--- topic/settings.py
+++ topic/settings.py
@@ -76,13 +76,13 @@ WSGI_APPLICATION = "topic.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'topic',
-'HOST': '127.0.0.1',
-'USER': 'root',
-'PASSWORD': '1234',
-'PORT': '3306',
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'topic',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'PORT': '3306',
 }
 }
 
